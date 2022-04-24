b90977a52db39550c9a6c8f25ad5758a60c8c2b9
Tiger Wang <tigerwang@outlook.com>
2022-04-24 18:59:41 -0400

    wip

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,3 +1,9 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+//
+const fs = require('fs');
+const infuraKey = fs.readFileSync(".key").toString().trim();
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -5,7 +11,15 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       websockets: true
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   compilers: {
     solc: {
